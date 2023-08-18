e5752a3785dc5e2c32c20f269f032483c287a8b2
Periphanes <johnwon0723@gmail.com>
2023-08-18 15:25:23 +0900

    minor updates

--- mooc_anom_inj.py
+++ mooc_anom_inj.py
@@ -14,13 +14,13 @@ with open('datasets/act-mooc/mooc_action_pickle.pickle', 'rb') as handle:
 # Timestamp - Seconds
 
 # static_data = data[:, :350000]
-static_data = data[:, :4000]
+static_data = data[:, 4000:5000]
 
 static_feats = np.concatenate((static_data[:2], np.expand_dims(static_data[8], 0)))
 static_labels = static_data[3]
 
 # online_data = data[:, 350000:]
-online_data = data[:, 4000:6000]
+online_data = data[:, 5000:40000]
 
 
 online_feats = np.concatenate((online_data[:2], np.expand_dims(online_data[8], 0)))
@@ -30,19 +30,20 @@ online_feats = np.concatenate((online_data[:2], np.expand_dims(online_data[8], 0
 
 print(static_feats.shape, static_labels.shape, online_feats.shape)
 
-online_len_dec = online_feats.shape[1] // 11
+online_len_dec = online_feats.shape[1] // 101
 
-anom_nodes = [0, 1, 2, 3, 4,6556,4324,524,643,1435,2343]
+anom_nodes = [random.randint(0,500) for _ in range(100)]
+print(anom_nodes)
 
-anom_count = 300
+anom_count = 100
 anom_ones = np.ones((anom_count))
 
 total_online_labels = np.zeros((online_len_dec))
 total_online_feats = online_feats[:, :online_len_dec]
 
-for i in range(10):
+for i in range(100):
     anom_node = anom_nodes[i]
-    targ_node = random.randint(10, 90)
+    targ_node = random.randint(0, 90)
 
     k = np.ones((3, anom_count))
     k[0] = anom_node
